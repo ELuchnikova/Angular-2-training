675b643c418e27e0d10407816c437a3976352a31
ekaterina_luchnikova@epam.com <ekaterina_luchnikova@epam.com>
2016-11-29 14:15:12 +0300

    task 1 after rework

---
+++ Task 1/src/containers.ts
@@ -0,0 +1,3 @@
+export const LIST_BODY = document.getElementById('list');
+export const LIST_NAVIGATION = document.getElementById('pagination');
+export const MAP_CONTAINER = document.getElementById('map');
